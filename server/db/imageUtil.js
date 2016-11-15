60d3425920b0aaf9a67374e40f1d21c1a961a263
Stephen Cefali <stephencefali@gmail.com>
2016-11-14 20:04:39 -0800

    converts to a general clarifai model

--- server/db/imageUtil.js
+++ server/db/imageUtil.js
@@ -5,8 +5,14 @@ const secret = require('./../secret');
 const fs = require('fs'); 
 const s3 = require('./../s3config');
 const Promise = require('bluebird');
-const trainerUtil = require('./../imageTrainer/trainerUtil');
+// const trainerUtil = require('./../imageTrainer/trainerUtil');
+const basicTrainerUtil = require('./../imageTrainer/basicTrainerUtil');
+const nameMappings = require('./../imageTrainer/nameMappings');
 
+
+
+const tags2check = basicTrainerUtil.readTags();
+const items2check = basicTrainerUtil.getItems();
 
 
 
@@ -22,8 +28,9 @@ clarApp.getToken().then( token => {
   tokenResponse = token;
 });
 
+
 var clarifaiModel;
-trainerUtil.getModel().then( model => {
+basicTrainerUtil.getModel().then( model => {
   clarifaiModel = model;
 });
 
@@ -65,18 +72,46 @@ exports.getClarifaiToken = () => {
 };
 
 
-exports.predict = (imageUrl) => {
-  //trainerUtil.listAllModels();
-  //return clarApp.models.predict(Clarifai.GENERAL_MODEL, imageUrl);
-  //return clarifaiModel.predict(imageUrl);
-  return clarifaiModel.train().then(
-    response =>{
-      return clarifaiModel.predict(imageUrl);
-    },
-    err => {
-      console.log('err training');
+var getMatches = tag => {
+  var checkList = [tag];
+  if (tag in nameMappings) {
+    checkList.push(nameMappings[tag]);
+  }
+  var matches = [];
+  items2check.forEach( item => {
+    var lowered = item.toLowerCase();
+    for (var i = 0; i < checkList.length; i ++) {
+      var tagcheck = checkList[i];
+      console.log('check', tagcheck, item);
+      if (lowered.includes(tagcheck)) {
+        matches.push(item);
+        break;
+      }
     }
-  );
+  });
+  return matches;
+}
+
+
+exports.predict = imageUrl => {
+  //return clarifaiModel.predict(imageUrl);
+  return clarApp.models.predict(Clarifai.GENERAL_MODEL, imageUrl).then( response => {
+    const data = response.data.outputs[0].data.concepts;
+    console.log('response', data);
+    var possibilities = [];
+    for (var i = 0; i < data.length; i ++) {
+      var tag = data[i].name;
+      var result = getMatches(tag);
+      possibilities = possibilities.concat(result);
+    }
+    console.log('possibilities', possibilities);
+    return possibilities;
+  },
+  err => {
+    throw err;
+  });
 };
+
+
 
 
---
+++ server/imageTrainer/nameMappings.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+  drawer: 'dresser',
+  laundry: 'washing - machine',
+  blowing: 'fan',
+  'electric fan': 'fan',
+  bike: 'bicycle',
+  luggage: 'suitcase',
+  television: 'tv',
+  barbecue: 'bbq grill',
+  container: 'toolchest',
+  luggage: 'cooler',
+  seat: 'chair',
+};
