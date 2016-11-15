595fbc6180156f28c4452bc52903d57a214c7991
Erik Suds <ErikSuds@Arlenes-MacBook-Air.local>
2016-11-15 09:45:27 -0800

    Merge client folder

---
+++ client/src/components/VideoFeed/styles.js
@@ -0,0 +1,25 @@
+export default {
+  localStream: {
+  	width: 320,
+  	height: 180,
+  	position: 'absolute',
+  	top: 350,
+  	left: 30
+  },
+  remoteStream: {
+  	width: 500,
+  	height: 360
+  },
+  imageContainer: {
+  	width: 400,
+  	height: 350
+  },
+  'imageContainer img': {
+  	'max-width': '100%'
+  },
+  'videoFeed': {
+  	'margin':'0 auto'
+  }
+};
+
+
