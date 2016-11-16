b0b37d0181589c2b17ae198c1d91078dfc1907e0
Stephen Cefali <stephencefali@gmail.com>
2016-11-15 17:29:06 -0800

    fixes image recognition

--- client/util/util.js
+++ client/util/util.js
@@ -5,17 +5,20 @@ const port = '9000'
 /************************************ URLS ************************************/
 const postCroppedImageURL = `http://${ip}:${port}/api/item/croppedImage`;
 const getClarifaiTokenURL = `http://${ip}:${port}/api/auth/clarifaiToken`;
-const postImageToClarifaiURL = `https://api.clarifai.com/v1/tag/`;
+//const postImageToClarifaiURL = `https://api.clarifai.com/v1/tag/`;
+const postImageToClarifaiURL = `https://api.clarifai.com/v2/tag/`;
 const getClarifaiInfoURL = `http://${ip}:${port}/api/auth/clarifaiInfo`;
 
 
 
 
 /************************************ PHOTOS ************************************/
+var clarApp;
 var clarifaiTags;
 var clarifaiToken;
 var clarifaiItems;
 var nameMappings;
+
 
 
 const postCroppedImage = (image) => {
@@ -36,6 +39,12 @@ const getClarifaiInfo = () => {
 		clarifaiToken = data.clarifaiToken;
 		clarifaiItems = data.clarifaiItems;
 		nameMappings = data.nameMappings;
+
+    clarApp = new Clarifai.App(
+      data.ClarifaiClientId,
+      data.ClarifaiClientSecret
+    );
+
 		return data;
 	})
 	.catch( error => {
@@ -51,11 +60,11 @@ const getMatches = tag => {
     checkList.push(nameMappings[tag]);
   }
   var matches = [];
-  items2check.forEach( item => {
-    var lowered = item.toLowerCase();
+  clarifaiTags.forEach( item => {
+    var lowered = item.toLowerCase().split(' ');
     for (var i = 0; i < checkList.length; i ++) {
       var tagcheck = checkList[i];
-      if (lowered.includes(tagcheck)) {
+      if (lowered.indexOf(tagcheck) > -1) {
         matches.push(item);
         break;
       }
@@ -68,8 +77,9 @@ const getMatches = tag => {
 const predict = (concepts) => {
   var possibilities = [];
   for (var i = 0; i < concepts.length; i ++) {
-    // var tag = concepts[i].name;
-    var tag = concepts[i];
+    var tag = concepts[i].name;
+    //var tag = concepts[i];
+    console.log('tag', tag);
     var result = getMatches(tag);
     possibilities = possibilities.concat(result);
   }
@@ -81,27 +91,39 @@ const predict = (concepts) => {
 
 
 const postImageToClarifai = (base64Image) => {
-	return axios(postImageToClarifaiURL, {
-		  method: 'post',
-		  model: 'general-v1.3',
-		  headers: {
-		    'Authorization': `Bearer ${clarifaiToken}`
-		  },
-		  data: {
-		  	'encoded_data': base64Image
-		  }
-		})
-	  .then(function (response) {
-	    console.log(response, 'response from inside utils');
-	    // const concepts = response.data.outputs[0].data.concepts;
-	    const concepts = response.data.results[0].result.tag.classes;
+	// return axios(postImageToClarifaiURL, {
+	// 	  method: 'post',
+	// 	  model: 'general-v1.3',
+	// 	  headers: {
+	// 	    'Authorization': `Bearer ${clarifaiToken}`
+	// 	  },
+	// 	  data: {
+	// 	  	'encoded_data': base64Image
+	// 	  }
+	// 	})
+	//   .then(function (response) {
+	//     console.log(response, 'response from inside utils');
+	//     // const concepts = response.data.outputs[0].data.concepts;
+	//     const concepts = response.data.results[0].result.tag.classes;
 
-	    return predict(concepts);
-	  })
-	  .catch(function (error) {
-	    console.log('Error postImageToClarifai:', error);
-	    throw error;
-	  });
+	//     return predict(concepts);
+	//   })
+	//   .catch(function (error) {
+	//     console.log('Error postImageToClarifai:', error);
+	//     throw error;
+	//   });
+
+
+  return  clarApp.models.predict(Clarifai.GENERAL_MODEL, {base64: base64Image}).then(
+    (response) => {
+      const concepts = response.data.outputs[0].data.concepts;
+      return predict(concepts);
+    },
+    (err) => {
+      console.error('predict err', err);
+      throw err;
+    }
+  );
 }
 
 
