656aff1fdeeb9999fd76c69520a696b1d239d1a4
Joe <josephsanglee@gmail.com>
2016-11-29 14:45:40 -0800

    Abstract socket connection code to sockets.js file

--- server/server.js
+++ server/server.js
@@ -22,55 +22,9 @@ if (process.env.LOCAL) {
   server = http.createServer(app);
 }
 
-const roomList = {};
-
 server.listen(port, () => console.log('server up and running at %s port', port));
-var io = require('socket.io')(server);
 
-const socketIdsInRoom = (name) => {
-  var socketIds = io.nsps['/'].adapter.rooms[name];
-  if (socketIds) {
-    var collection = [];
-    for (var key in socketIds) {
-      collection.push(key);
-    }
-    return collection;
-  } 
-
-  return [];
-};
-
-io.on('connection', function(socket){
-  console.log('connection');
-  socket.on('disconnect', function(){
-    console.log('disconnect');
-    if (socket.room) {
-      var room = socket.room;
-      io.to(room).emit('leave', socket.id);
-      socket.leave(room);
-    }
-  });
-
-  socket.on('join', function(name, callback){
-    console.log('join', name);
-    var socketIds = socketIdsInRoom(name);
-    callback(socketIds);
-    socket.join(name);
-    socket.room = name;
-  });
-
-
-  socket.on('exchange', function(data){
-    console.log('exchange', data);
-    data.from = socket.id;
-    console.log('data.from', data.from);
-    console.log('io.sockets.connected', io.sockets.connected);
-    console.log('data.to', data.to);
-    var to = io.sockets.connected[data.to];
-    to.emit('exchange', data);
-  });
-});
-
+require('./sockets')(server);
 require('./middleware')(app, express, server);
 require('./router')(app, express, server);
 
