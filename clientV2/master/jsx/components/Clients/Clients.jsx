576f7664c7dd360f6c231e90bd382bbb44144671
Erik Suddath <eriksudd@gmail.com>
2016-11-29 18:56:06 -0800

    implement searchable clients

--- clientV2/master/jsx/components/Clients/Clients.jsx
+++ clientV2/master/jsx/components/Clients/Clients.jsx
@@ -22,24 +22,43 @@ class Clients extends React.Component {
     }
 
     getAllMoves() {
-      util.getAllMoves().then(moves => this.setState({ moves }))
+      util.getAllMoves().then(moves => {
+        this.setState({ moves });
+        this.moves = moves;
+      })
     }
 
     onTableClick(user_id) {
       // redirect to user profile
-      console.log('rerouting to:', user_id)
       const path = `/userProfile/${user_id}`;
       browserHistory.push(path);
     }
 
+    filterMoves(query) {
+      let moves;
+      if (query.length > 0) {
+        moves = this.moves.filter( move => {
+          let moveString = JSON.stringify(move).toLowerCase();
+          return moveString.includes(query.toLowerCase())
+        })
+      } else {
+        moves = this.moves;
+      }
+      console.log('setting moves to', moves);
+      this.setState({ moves })
+    }
+
     render() {
       const { moves } = this.state;
-      const { onTableClick } = this;
+      const { onTableClick, filterMoves } = this;
       console.log(moves, 'moves')
       return (
           <ContentWrapper>
             <Row>                 
-                <ClientTable moves={ moves } onTableClick={ onTableClick } />
+                <ClientTable 
+                  moves={ moves } 
+                  onTableClick={ onTableClick } 
+                  filterMoves={ filterMoves.bind(this) }/>
             </Row>
           </ContentWrapper>
       );
