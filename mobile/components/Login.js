9a7e1b298ec0f664f0e4d8cbad3b7731805512f6
Joe <josephsanglee@gmail.com>
2016-12-01 15:51:12 -0800

    Modify styling for Login component

--- mobile/components/Login.js
+++ mobile/components/Login.js
@@ -7,7 +7,15 @@ import {
   TouchableHighlight,
   AlertIOS,
   Dimensions,
+  Image,
 } from 'react-native';
+import {
+  List,
+  ListItem,
+  InputGroup,
+  Input,
+  Button,
+} from 'native-base';
 import t from 'tcomb-form-native';
 
 import Signup from './Signup';
@@ -44,7 +52,11 @@ const onValueChange = async (item, selectedValue) => {
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.userInfo = null;
+
+    this.state = {
+      email: '',
+      password: '',
+    }
   }
 
   goToNext(type) {
@@ -60,61 +72,67 @@ export default class Login extends React.Component {
   }
 
   userAuth(type) {
-    const value = this.refs.form.getValue();
+    const user = {
+      username: this.state.email,
+      password: this.state.password,
+    };
 
-    if (value) {
-      const user = {
-        username: value.email,
-        password: value.password,
-      };
+    helper.postUser(user, type)
+    .then((response) => {
+      const moveData = response.data.lastMove;
 
-      helper.postUser(user, type)
-      .then((response) => {
-        const moveData = response.data.lastMove;
+      if (moveData) {
+        const moveId = moveData._id;
+        onValueChange('moveId', moveId);
+        onValueChange('moveData', JSON.stringify(moveData));
+      }
 
-        if (moveData) {
-          const moveId = moveData._id;
-          onValueChange('moveId', moveId);
-          onValueChange('moveData', JSON.stringify(moveData));
-        }
-
-        this.goToNext(type);
-      })
-      .catch((error) => {
-        console.log(error);
-        AlertIOS.alert('Invalid username or password. Please try again.');
-      });
-    }
+      this.goToNext(type);
+    })
+    .catch((error) => {
+      console.log(error);
+      AlertIOS.alert('Invalid username or password. Please try again.');
+    });
   }
 
   render() {
     return (
       <View style={styles.main} flexDirection="column">
-        <View style={styles.row}>
-          <Text style={styles.title}>MoveKick</Text>
+        <View>
+          <Image style={styles.logo}
+            source={require('../assets/images/mkLogo.png')}
+          />
         </View>
         <View style={styles.container}>
-          <View style={styles.row}>
-            <Form
-              ref="form"
-              style={{borderColor: 'red'}}
-              type={Person}
-              options={options}
-            />
+          <View>
+              <InputGroup borderType="regular">
+                <Input
+                  autoCapitalize="none"
+                  placeholder="Email"
+                  onChangeText={email => this.setState({ email })}
+                />
+              </InputGroup>
+              <InputGroup borderType="regular">
+                <Input
+                  secureTextEntry={true}
+                  placeholder="Password"
+                  onChangeText={password => this.setState({ password })}
+                />
+              </InputGroup>
           </View>
-          <View style={styles.row}>
-            <TouchableHighlight
-              style={styles.button}
+          <View style={styles.login}>
+            <Button
+              block
+              primary
               onPress={() => this.userAuth('signin')}
-              underlayColor="limegreen"
             >
-              <Text style={styles.buttonText}>Login</Text>
-            </TouchableHighlight>
+              Login
+            </Button>
           </View>
         </View>
-        <View style={styles.signup}>
+        <TouchableHighlight style={styles.signup}>
           <Text onPress={() => this.goToNext('signup')}>Don't have an account? Sign up here!</Text>
-        </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -124,52 +142,27 @@ export default class Login extends React.Component {
 
 const width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    flexDirection: 'column',
+  login: {
+    top: 30,
   },
   container: {
     justifyContent: 'center',
-    // alignItems: 'center',
     alignSelf: 'center',
-    marginTop: 50,
+    marginTop: 40,
     width: width * 0.9,
     borderRadius: 5,
     flexDirection: 'column',
-    // width: 150,
-    // height: 300,
     padding: 20,
     backgroundColor: 'rgba(255,255,255,.85)',
   },
-  title: {
-    fontSize: 50,
-    fontFamily: 'Futura',
-    marginTop: 50,
-    backgroundColor: 'transparent',
-    alignSelf: 'center',
-    marginBottom: 30,
-    opacity: 1,
-    textAlign: 'center',
-    textShadowColor: 'black',
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center',
-  },
-  button: {
-    height: 36,
-    backgroundColor: '#1E90FF',
-    borderColor: '#1E90FF',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    top: 50,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-  },
   signup: {
-    top: 150,
+    top: 120,
+    alignSelf: 'center',
+  },
+  logo: {
+    top: 30,
+    width: 300,
+    height: 300,
     alignSelf: 'center',
   },
 });
